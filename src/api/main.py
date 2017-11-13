d8254717586c61021c8993ac25aa7d927792c209
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2017-11-13 19:01:34 -0200

    Adding Highchart with request information in real time on dashboard

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,10 @@
 from base import api
 from base import app
+from flask_cors import CORS
 import config
 import api
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == '__main__':
     app.run(
