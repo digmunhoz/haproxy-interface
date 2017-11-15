86556c31110d8fe52726ada127d9849b42e1d6a3
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2017-11-15 00:55:10 -0200

    New itens into dashboard

--- src/web/web.py
+++ src/web/web.py
@@ -8,37 +8,52 @@ import config
 
 @app.route("/home", methods=['GET'])
 def home():
-    hardware = requests.get('http://' + 
-                            config.api_server['ADDRESS'] + ':' + 
-                            config.api_server['PORT'] + 
-                            '/api/hardware', 
-                            timeout=config.connection_timeout
+
+    hardware = requests.get('http://{}:{}{}'.format(
+        config.api_server['ADDRESS'], 
+        config.api_server['PORT'], 
+        '/api/hardware'), timeout=config.connection_timeout
     ).json()
-    info = requests.get('http://' + 
-                        config.api_server['ADDRESS'] + ':' + 
-                        config.api_server['PORT'] + 
-                        '/api/info', 
-                        timeout=config.connection_timeout
+
+    info = requests.get('http://{}:{}{}'.format( 
+        config.api_server['ADDRESS'], 
+        config.api_server['PORT'], 
+        '/api/info'), timeout=config.connection_timeout
     ).json()
-    servers = requests.get('http://' + 
-                        config.api_server['ADDRESS'] + ':' + 
-                        config.api_server['PORT'] + 
-                        '/api/servers', 
-                        timeout=config.connection_timeout
+
+    servers = requests.get('http://{}:{}{}'.format(
+        config.api_server['ADDRESS'], 
+        config.api_server['PORT'], 
+        '/api/servers'), timeout=config.connection_timeout
     ).json()
-    servers_up = [x for x in servers if x['status'] == 'UP']
-    servers_down = [x for x in servers if x['status'] != 'UP']
+
+    backends = requests.get('http://{}:{}{}'.format(
+        config.api_server['ADDRESS'], 
+        config.api_server['PORT'], 
+        '/api/backends'), timeout=config.connection_timeout
+    ).json()
+
+    servers_up = [s for s in servers if s['status'] == 'UP']
+    servers_down = [s for s in servers if s['status'] != 'UP']
     
+    backends_up = [b for b in backends if b['status'] == 'UP']
+    backends_down = [b for b in backends if b['status'] != 'UP']
+
     cpu = hardware['cpu']
     mem = hardware['mem']
     currconns = info[0]['CurrConns']
     version = info[0]['Version']
-    name = info[0]['Name']
+    haproxy_name = info[0]['node']
     uptime = info[0]['Uptime']
+    ulimitn = info[0]['Ulimit-n']    
     maxconn = int(info[0]['Hard_maxconn'])
+    maxsessrate = int(info[0]['MaxSessRate'])
+    sessrate = int(info[0]['SessRate'])
     maxconn = '{0:,}'.format(maxconn).replace(',','.')
     servers_up = len(servers_up)
     servers_down = len(servers_down)
+    backends_up = len(backends_up)
+    backends_down = len(backends_down)    
     return render_template('home.html', 
                             cpu=cpu, 
                             mem=mem,
@@ -46,7 +61,11 @@ def home():
                             version=version,
                             maxconn=maxconn,
                             uptime=uptime,
-                            name=name,
+                            haproxy_name=haproxy_name,
                             servers_up=servers_up,
-                            servers_down=servers_down
+                            servers_down=servers_down,
+                            backends_up=backends_up,
+                            backends_down=backends_down,
+                            sessrate=sessrate,
+                            ulimitn=ulimitn
     )
