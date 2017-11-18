58b29bc06fef4dc31deb9a12baff322a89af38dc
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2017-11-18 19:06:53 -0200

    Add root path redirect to dashboard

--- src/web/main.py
+++ src/web/main.py
@@ -1,7 +1,12 @@
+from flask import Flask, redirect
 from base import app
 import dashboard
 import servers
 import config
+
+@app.route('/')
+def hello():
+    return redirect('/dashboard')
 
 if __name__ == '__main__':
     app.run(
