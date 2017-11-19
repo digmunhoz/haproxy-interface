b160f1cd5df13c43ada596b5cbf2ad8de4065330
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2017-11-18 22:38:43 -0200

    Add links to server page on button UP and DOWN on dashboard

--- src/web/servers.py
+++ src/web/servers.py
@@ -10,28 +10,10 @@ import config
 @app.route("/servers", methods=['GET'])
 def servers():
 
-    hardware = requests.get('http://{}:{}{}'.format(
-        config.api_server['ADDRESS'], 
-        config.api_server['PORT'], 
-        config.api_endpoint['HARDWARE']), timeout=config.connection_timeout
-    ).json()
-
-    info = requests.get('http://{}:{}{}'.format( 
-        config.api_server['ADDRESS'], 
-        config.api_server['PORT'], 
-        config.api_endpoint['INFO']), timeout=config.connection_timeout
-    ).json()
-
     servers = requests.get('http://{}:{}{}'.format(
         config.api_server['ADDRESS'], 
         config.api_server['PORT'], 
         config.api_endpoint['SERVERS']), timeout=config.connection_timeout
-    ).json()
-
-    frontends = requests.get('http://{}:{}{}'.format(
-        config.api_server['ADDRESS'], 
-        config.api_server['PORT'], 
-        config.api_endpoint['FRONTENDS']), timeout=config.connection_timeout
     ).json()
 
     backends = requests.get('http://{}:{}{}'.format(
@@ -46,37 +28,59 @@ def servers():
     backends_up = [b for b in backends if b['status'] == 'UP']
     backends_down = [b for b in backends if b['status'] != 'UP']
 
-    cpu = hardware['cpu']
-    mem = hardware['mem']
-    currconns = info[0]['CurrConns']
-    version = info[0]['Version']
-    node = info[0]['node']
-    uptime = info[0]['Uptime']
-    ulimitn = info[0]['Ulimit-n']    
-    maxconn = int(info[0]['Hard_maxconn'])
-    maxsessrate = int(info[0]['MaxSessRate'])
-    sessrate = int(info[0]['SessRate'])
-    maxconn = '{0:,}'.format(maxconn).replace(',','.')
     servers_up = len(servers_up)
     servers_down = len(servers_down)
     backends_up = len(backends_up)
     backends_down = len(backends_down)
 
-    return render_template('servers.html', 
-                            cpu=cpu, 
-                            mem=mem,
-                            currconns=currconns,
-                            version=version,
-                            maxconn=maxconn,
-                            uptime=uptime,
-                            node=node,
+    return render_template('servers.html',
                             servers_up=servers_up,
                             servers_down=servers_down,
                             backends_up=backends_up,
                             backends_down=backends_down,
-                            sessrate=sessrate,
-                            ulimitn=ulimitn,
                             servers=servers,
-                            frontends=frontends,
                             backends=backends,
     )
+
+
+@app.route("/servers_page", methods=['GET'])
+def servers_page():
+
+    servers = requests.get('http://{}:{}{}'.format(
+        config.api_server['ADDRESS'], 
+        config.api_server['PORT'], 
+        config.api_endpoint['SERVERS']), timeout=config.connection_timeout
+    ).json()
+
+    servers_up = [s for s in servers if s['status'] == 'UP']
+    servers_down = [s for s in servers if s['status'] != 'UP']
+
+    servers_up = len(servers_up)
+    servers_down = len(servers_down)
+
+    return render_template('servers_page.html',
+                            servers_up=servers_up,
+                            servers_down=servers_down,
+                            servers=servers,
+    )
+
+@app.route("/backends_page", methods=['GET'])
+def backends_page():
+
+    backends = requests.get('http://{}:{}{}'.format(
+        config.api_server['ADDRESS'], 
+        config.api_server['PORT'], 
+        config.api_endpoint['BACKENDS']), timeout=config.connection_timeout
+    ).json()
+    
+    backends_up = [b for b in backends if b['status'] == 'UP']
+    backends_down = [b for b in backends if b['status'] != 'UP']
+
+    backends_up = len(backends_up)
+    backends_down = len(backends_down)
+
+    return render_template('backends_page.html',
+                            backends_up=backends_up,
+                            backends_down=backends_down,
+                            backends=backends,
+    )    
